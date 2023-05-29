9bf929ea1ea86b9c3429ec237888a37d3c6d3ae8
shulevskyi <dshulevskyi@kse.org.ua>
2023-05-29 16:18:02 +0300

    Update initial.R

--- initial.R
+++ initial.R
@@ -16,33 +16,30 @@ holidays_events_data <- read.csv(url_holidays_events)
 transactions_data <- read.csv(url_transactions)
 
 # Changing the order for $id
+
 train_data$id <- seq_len(nrow(train_data)) - 1
 
 
 # Filtering by date
+
 holidays_events_data <- holidays_events_data[holidays_events_data$date >= "2014-01-07", ]
 oil_data <- oil_data[oil_data$date >= "2014-01-07", ]
 transactions_data <- transactions_data[transactions_data$date >= "2014-01-07", ]
 
-head(train_data)
-head(stores_data)
-head(oil_data)
-head(holidays_events_data)
+# Merging train_data, stores_data & holidays_events_data
 
-# Merging  by store_nbr
+train_df <- merge(train_data, stores_data, by = 'store_nbr')
+train_df <- merge(train_df, oil_data, by = 'date', all.x = TRUE)
+colnames(holidays_events_data)[colnames(holidays_events_data) == 'type'] <- 'holiday_type'
+train_df <- merge(train_df, holidays_events_data, by = 'date', all.x = TRUE)
 
-merged_data <- merge(train_data, stores_data, by = "store_nbr")
-merged_data <- merge(train_data, oil_data, by = "date")
+# Calculate the percentage of missing values in each column
 
-# Get a sample of 100 observations from transactions_data
+missing_percent <- colMeans(is.na(train_df)) * 100
+print(missing_percent)
+columns_to_remove <- names(train_df)[missing_percent > 30]
+train_df <- train_df[, !names(train_df) %in% columns_to_remove]
 
-library(dplyr)
-
-merged_data_s <- head(merged_data, 100)
-merged_data_f <- merge(merged_data_s, transactions_data, by = "store_nbr")
-
-
-head(merged_data_f)
 
 
 
